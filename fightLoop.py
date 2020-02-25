9f5cfb249c896c4b8a0090a95c0ebd57d0d000c1
Jonathan <42983141+JonathanCote90@users.noreply.github.com>
2020-02-25 11:43:18 -0700

    First iteration of fight loop
    Initially written to test out major differences between Matlab and Python. Not Very useful thing.

---
+++ fightLoop.py
@@ -0,0 +1,13 @@
+from random import randint
+
+combatant1HP = 10
+combatant2HP = 10
+
+while combatant1HP > 0 & combatant2HP > 0:
+    combatant1HP - randint(0, 3)
+    combatant2HP - randint(0, 3)
+
+if combatant1HP <= 0:
+    print("Winner is combatant 2")
+else:
+    print("Winner is combatant 1")
