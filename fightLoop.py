2c1d717466295d5149930661084a69949b228ac3
JonathanCote90 <cote.jonathan@gmail.com>
2020-02-25 12:32:49 -0700

    HP should now update after each attack. Value change was missing.

--- fightLoop.py
+++ fightLoop.py
@@ -4,8 +4,8 @@ combatant1HP = 10
 combatant2HP = 10
 
 while combatant1HP > 0 & combatant2HP > 0:
-    combatant1HP - randint(0, 3)
-    combatant2HP - randint(0, 3)
+    combatant1HP = combatant1HP - randint(0, 3)
+    combatant2HP = combatant2HP - randint(0, 3)
 
 if combatant1HP <= 0:
     print("Winner is combatant 2")
