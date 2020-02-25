edcbd69cf530f6c373437c5f5e57c25f84588116
Gownsan <gownsan@gmail.com>
2020-02-25 16:47:16 -0500

    Uploading PyCharm initial repository ...again

--- fightLoop.py
+++ fightLoop.py
@@ -2,6 +2,7 @@
 from random import randint
 from dataclasses import dataclass
 
+
 # defining classes
 @dataclass
 class Stats:
@@ -69,6 +70,7 @@ fighter = Job(
     startHP=21
 )
 
+
 # defining function to create combatants from combination of origin and job
 def create_combatant(chosen_origin, chosen_job):
     combatant = Combatant(
@@ -83,13 +85,29 @@ def create_combatant(chosen_origin, chosen_job):
 combatant1 = create_combatant(knight, fighter)
 combatant2 = create_combatant(knight, fighter)
 
-while combatant1.currentHP > 0 and combatant2.currentHP > 0:
-    combatant1.currentHP = combatant1.currentHP - randint(1, 8)
-    combatant2.currentHP = combatant2.currentHP - randint(1, 8)
-    print("Combatant 1 has " + str(combatant1.currentHP) +
-          " HP left and Combatant 2 has " + str(combatant2.currentHP) + " HP left.")
+combatant1_wins = 0
+combatant2_wins = 0
+double_kill = 0
+iteration = 10000
 
-if combatant1.currentHP <= 0:
-    print("Winner is combatant 2")
-else:
-    print("Winner is combatant 1")
+for i in range(1, iteration):
+    # print(i)
+    combatant1.currentHP = combatant1.maxHP
+    combatant2.currentHP = combatant2.maxHP
+
+    while combatant1.currentHP > 0 and combatant2.currentHP > 0:
+        combatant1.currentHP = combatant1.currentHP - randint(1, 8)
+        combatant2.currentHP = combatant2.currentHP - randint(1, 8)
+        # print("Combatant 1 has " + str(combatant1.currentHP) +
+        #      " HP left and Combatant 2 has " + str(combatant2.currentHP) + " HP left.")
+
+    if combatant2.currentHP <= 0 and combatant1.currentHP <= 0:
+        double_kill = double_kill + 1
+    elif combatant2.currentHP <= 0:
+        combatant1_wins = combatant1_wins + 1
+    else:
+        combatant2_wins = combatant2_wins + 1
+
+print(combatant1_wins/iteration*100)
+print(combatant2_wins/iteration*100)
+print(double_kill/iteration*100)
