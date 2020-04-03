e82d61fedfc17012f08087dcefa11bcb2587fd28
JonathanCote90 <cote.jonathan@gmail.com>
2020-04-03 00:41:00 -0600

    Iterator started: bug at damage

--- fightLoop.py
+++ fightLoop.py
@@ -5,54 +5,65 @@ from math import floor
 
 
 # defining classes
-@dataclass
+# @dataclass
 class Stats:
-    STR: int
-    DEX: int
-    AGI: int
-    VIT: int
-    SPR: int
-    MND: int
+    def __init__(self, STR: int, DEX: int, AGI: int, VIT: int, SPR: int, MND: int):
+        self.STR = STR
+        self.DEX = DEX
+        self.AGI = AGI
+        self.VIT = VIT
+        self.SPR = SPR
+        self.MND = MND
 
 
-@dataclass
+#@dataclass
 class DmgType:
-    slash: int
-    pierce: int
-    blunt: int
-    magic: int
-    fire: int
-    cold: int
-    toxic: int
-    bleed: int
-    lightning: int
-    dark: int
+    def __init__(self, slash: int, pierce: int, blunt: int, magic: int, fire: int, cold: int, toxic: int, bleed: int, lightning: int, dark: int):
+        self.slash = slash
+        self.pierce = pierce
+        self.blunt = blunt
+        self.magic = magic
+        self.fire = fire
+        self.cold = cold
+        self.toxic = toxic
+        self.bleed = bleed
+        self.lightning = lightning
+        self.dark = dark
 
 
-@dataclass
+#@dataclass
 class Origin:
-    name: str
-    stats: Stats
-    block: DmgType
-    resist: DmgType
-    dodge: int
-    weapon: DmgType
-    encumbrance: int
+    def __init__(self, name: str, stats: Stats, block: DmgType, resist: DmgType, dodge: int, weapon: DmgType, encumbrance: int, passedList: list):
+        self.name = name
+        self.stats = stats
+        self.block = block
+        self.resist = resist
+        self.dodge = dodge
+        self.weapon = weapon
+        self.encumbrance = encumbrance
+        self.passedList = passedList.append(self)
 
 
-@dataclass
+#@dataclass
 class Job:
-    name: str
-    startHP: int
+    def __init__(self, name: str, startHP: int, passedList: list):
+        self.name = name
+        self.startHP = startHP
+        self.passedList = passedList.append(self)
 
 
-@dataclass
+#@dataclass
 class Combatant:
-    origin: Origin
-    job: Job
-    maxHP: int
-    currentHP: int
+    def __init__(self, origin: Origin, job: Job, maxHP: int, currentHP: int):
+        self.origin = origin
+        self.job = job
+        self.maxHP = maxHP
+        self.currentHP = currentHP
 
+
+listOfOrigins = [] # listOfOrigins will contain the list of all 
+# created origins so the script can itterate through it
+listOfJobs = [] # Idem for jobs
 
 # defining origins
 knight = Origin(
@@ -62,7 +73,8 @@ knight = Origin(
     resist=DmgType(5, 5, 3, 0, 0, 0, 0, 0, 0, 0),
     dodge=0,
     weapon=DmgType(8, 8, 0, 0, 0, 0, 0, 0, 0, 0),
-    encumbrance=3
+    encumbrance=3,
+    passedList= listOfOrigins # Needed to be passed on to the init function
 )
 
 soldier = Origin(
@@ -72,7 +84,8 @@ soldier = Origin(
     resist=DmgType(3, 3, 3, 0, 1, 0, 0, 0, 0, 0),
     dodge=1,
     weapon=DmgType(8, 8, 0, 0, 0, 0, 0, 0, 0, 0),
-    encumbrance=2
+    encumbrance=2,
+    passedList= listOfOrigins # Needed to be passed on to the init function
 )
 
 warrior = Origin(
@@ -82,7 +95,8 @@ warrior = Origin(
     resist=DmgType(2, 2, 2, 0, 0, 4, 0, 0, 0, 0),
     dodge=1,
     weapon=DmgType(8, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    encumbrance=2
+    encumbrance=2,
+    passedList= listOfOrigins # Needed to be passed on to the init function
 )
 
 thief = Origin(
@@ -92,7 +106,8 @@ thief = Origin(
     resist=DmgType(1, 1, 1, 0, 0, 0, 3, 0, 0, 0),
     dodge=2,
     weapon=DmgType(4, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    encumbrance=1
+    encumbrance=1,
+    passedList= listOfOrigins # Needed to be passed on to the init function
 )
 
 wanderer = Origin(
@@ -102,7 +117,8 @@ wanderer = Origin(
     resist=DmgType(0, 0, 0, 0, 0, 2, 1, 0, 0, 0),
     dodge=0,
     weapon=DmgType(6, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    encumbrance=0
+    encumbrance=0,
+    passedList= listOfOrigins # Needed to be passed on to the init function
 )
 
 herald = Origin(
@@ -112,7 +128,8 @@ herald = Origin(
     resist=DmgType(3, 3, 3, 0, 1, 1, 0, 0, 1, 0),
     dodge=1,
     weapon=DmgType(0, 6, 0, 0, 0, 0, 0, 0, 0, 0),
-    encumbrance=2
+    encumbrance=2,
+    passedList= listOfOrigins # Needed to be passed on to the init function
 )
 
 cleric = Origin(
@@ -122,7 +139,8 @@ cleric = Origin(
     resist=DmgType(0, 0, 0, 1, 0, 0, 1, 0, 1, 2),
     dodge=1,
     weapon=DmgType(0, 0, 6, 0, 0, 0, 0, 0, 0, 0),
-    encumbrance=0
+    encumbrance=0,
+    passedList= listOfOrigins # Needed to be passed on to the init function
 )
 
 sorcerer = Origin(
@@ -132,7 +150,8 @@ sorcerer = Origin(
     resist=DmgType(0, 0, 0, 3, 2, 1, 0, 0, 0, 0),
     dodge=2,
     weapon=DmgType(0, 0, 6, 0, 0, 0, 0, 0, 0, 0),
-    encumbrance=0
+    encumbrance=0,
+    passedList= listOfOrigins # Needed to be passed on to the init function
 )
 
 pyromancer = Origin(
@@ -142,29 +161,34 @@ pyromancer = Origin(
     resist=DmgType(0, 0, 0, 0, 4, 0, 1, 0, 0, 1),
     dodge=1,
     weapon=DmgType(6, 0, 0, 0, 0, 0, 0, 0, 0, 0),
-    encumbrance=0
+    encumbrance=0,
+    passedList= listOfOrigins # Needed to be passed on to the init function
 )
 
 
 # defining jobs
 barbarian = Job(
     name="barbarian",
-    startHP=24
+    startHP=24,
+    passedList= listOfJobs
 )
 
 fighter = Job(
     name="fighter",
-    startHP=21
+    startHP=21,
+    passedList= listOfJobs
 )
 
 monk = Job(
     name="monk",
-    startHP=18
+    startHP=18,
+    passedList= listOfJobs
 )
 
 rogue = Job(
     name="rogue",
-    startHP=18
+    startHP=18,
+    passedList= listOfJobs
 )
 
 
@@ -201,54 +225,80 @@ def initiative_roll(player0, player1):
     else:
         return randint(0,1)
 
+# Initializing the 4x forLoop 
+individualCombos = len(listOfOrigins) * len(listOfJobs)
+combatant1Origins = []
+combatant1Jobs = []
+combatant1VictoryRates = []
 
-# creating combatants and the roster of contenders
-combatant1 = create_combatant(knight, fighter)
-combatant2 = create_combatant(knight, fighter)
-pc = [combatant1, combatant2]
+comboNumber = 0 #To track which cmbt1 jobs x orgn number this is
 
-# initiating variables to store the outcomes
-combatant1_wins = 0
-combatant2_wins = 0
-double_kill = 0
+for origin1 in listOfOrigins:
+    for job1 in listOfJobs:
+        combatant1_total_wins = 0
 
-# specifying number of iterations
-iteration = 100000
+        # set up the list / table values
+        combatant1Origins.append(origin1.name)
+        combatant1Jobs.append(job1.name)
 
+        for origin2 in listOfOrigins:
+            for job2 in listOfJobs:
+                
+                # creating combatants and the roster of contenders
+                combatant1 = create_combatant(origin1, job1)
+                combatant2 = create_combatant(origin2, job2)
+                pc = [combatant1, combatant2]
 
-# let the games ...begin!
-for i in range(1, iteration):
+                # initiating variables to store the outcomes
+                combatant1_wins = 0
+                combatant2_wins = 0
+                double_kill = 0
 
-    # healing combatants to max hp
-    pc[0].currentHP = pc[0].maxHP
-    pc[1].currentHP = pc[1].maxHP
-
-    # roll initiative!
-    current_turn = initiative_roll(pc[0], pc[1])
-
-    # as long as both live, the battle rages on...
-    while pc[0].currentHP > 0 and combatant2.currentHP > 0:
-        # ...whose turn is it now?
-        active = current_turn
-        passive = abs(active - 1)
-        # active pc attacks!
-        attack = randint(1, 20) + mod(pc[active], "DEX")
-        # passive pc dodges...?
-        dodge = randint(1, 20) + mod(pc[passive], "AGI") + pc[passive].origin.dodge - pc[passive].origin.encumbrance
-        # if they didn't dodge, passive pc loses HP!
-        if attack >= dodge:
-            pc[passive].currentHP = pc[passive].currentHP - randint(1, pc[active].origin.weapon.slash) + mod(pc[active], "STR")
-        # end of active pc's turn
-        current_turn = abs(current_turn -1)
+                # specifying number of iterations
+                iteration = 10
 
 
-    if pc[1].currentHP <= 0 and pc[0].currentHP <= 0:
-        double_kill = double_kill + 1
-    elif pc[1].currentHP <= 0:
-        combatant1_wins = combatant1_wins + 1
-    else:
-        combatant2_wins = combatant2_wins + 1
+                # let the games ...begin!
+                for i in range(1, iteration):
 
-print(combatant1_wins / iteration * 100)
-print(combatant2_wins / iteration * 100)
-print(double_kill / iteration * 100)
+                    # healing combatants to max hp
+                    pc[0].currentHP = pc[0].maxHP
+                    pc[1].currentHP = pc[1].maxHP
+
+                    # roll initiative!
+                    current_turn = initiative_roll(pc[0], pc[1])
+
+                    # as long as both live, the battle rages on...
+                    while pc[0].currentHP > 0 and combatant2.currentHP > 0:
+                        # ...whose turn is it now?
+                        active = current_turn
+                        passive = abs(active - 1)
+                        # active pc attacks!
+                        attack = randint(1, 20) + mod(pc[active], "DEX")
+                        # passive pc dodges...?
+                        dodge = randint(1, 20) + mod(pc[passive], "AGI") + pc[passive].origin.dodge - pc[passive].origin.encumbrance
+                        # if they didn't dodge, passive pc loses HP!
+                        if attack >= dodge:
+                            #TODO Buggy line to fix with Guillaume
+                            print(f"Bug occurs at {origin2.name} and {job2.name}")
+                            pc[passive].currentHP = pc[passive].currentHP - randint(1, pc[active].origin.weapon.slash) + mod(pc[active], "STR")
+                        # end of active pc's turn
+                        current_turn = abs(current_turn -1)
+
+
+                    if pc[1].currentHP <= 0 and pc[0].currentHP <= 0:
+                        double_kill = double_kill + 1
+                    elif pc[1].currentHP <= 0:
+                        combatant1_total_wins = combatant1_total_wins +1
+                        #combatant1_wins = combatant1_wins + 1
+                    else:
+                        combatant2_wins = combatant2_wins + 1
+
+                #print(combatant1_wins / iteration * 100)
+                #print(combatant2_wins / iteration * 100)
+                #print(double_kill / iteration * 100)
+                
+
+        combatant1VictoryRates.append(combatant1_total_wins)
+        print(f"This was the {origin1.name} origin with a {listOfJobs.name} job...")
+        comboNumber = comboNumber + 1
